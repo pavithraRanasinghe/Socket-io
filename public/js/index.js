5564adba5f07557aadad5b98e76ccd90ded9ac84
pavithraRanasinghe <paviranasinghe20@gmail.com>
2020-01-29 10:18:05 +0530

    Add autoscroll to chat page

--- public/js/index.js
+++ public/js/index.js
@@ -1,10 +1,26 @@
 var socket = io();
 
-socket.on('connect',()=>{
-    console.log("Connect to server") ;
+function scrollToBottom() {
+    const messages = $('#messages');
+    const newMessage = messages.children('li:last-child');
+
+    const clientHeight = messages.prop('clientHeight');
+    const scrollTop = messages.prop('scrollTop');
+    const scrollHeight = messages.prop('scrollHeight');
+    const newMessageHeight = newMessage.innerHeight();
+    const lastMessageHeight = newMessage.prev().innerHeight();
+
+    if (scrollTop + clientHeight + newMessageHeight + lastMessageHeight >= scrollHeight) {
+        messages.scrollTop(scrollHeight);
+    }
+
+}
+
+socket.on('connect', () => {
+    console.log("Connect to server");
 });
 
-socket.on('newMessage',(message)=>{
+socket.on('newMessage', (message) => {
     const formattedTime = moment(message.createdAt).format('hh:mm:a');
     const template = $('#message-template').html();
     const html = Mustache.render(template, {
@@ -13,9 +29,10 @@ socket.on('newMessage',(message)=>{
         createdAt: formattedTime
     });
     $('#messages').append(html);
+    scrollToBottom();
 });
 
-socket.on('newLocationMessage',(message)=>{
+socket.on('newLocationMessage', (message) => {
     const formattedTime = moment(message.createdAt).format('hh:mm:a');
     const template = $('#location-message-template').html();
     const html = Mustache.render(template, {
@@ -24,44 +41,44 @@ socket.on('newLocationMessage',(message)=>{
         createdAt: formattedTime
     });
     $('#messages').append(html);
-
+    scrollToBottom();
 });
 
-socket.on('disconnect',()=>{
+socket.on('disconnect', () => {
     console.log("Disconnected from server");
 });
 
 $('#message-form').on('submit', function (e) {
     e.preventDefault();
 
-    const textBox =$('[name=message]');
+    const textBox = $('[name=message]');
 
-    socket.emit('createMessage',{
+    socket.emit('createMessage', {
         from: 'User',
         text: textBox.val()
-    },function () {
+    }, function () {
         return textBox.val('');
     });
 });
 
 const locationButton = $('#location');
 
-locationButton.on('click',function () {
-    if (!navigator.geolocation){
+locationButton.on('click', function () {
+    if (!navigator.geolocation) {
         return alert('Geolocation not supported by your browser');
     }
 
-    locationButton.attr('disabled','disabled').text('Sending Location...');
+    locationButton.attr('disabled', 'disabled').text('Sending Location...');
 
     navigator.geolocation.getCurrentPosition(function (position) {
         console.log(position);
         $('#location').removeAttr('disabled').text('Send Location');
 
-        socket.emit('createLocationMessage',{
+        socket.emit('createLocationMessage', {
             latitude: position.coords.latitude,
             longitude: position.coords.longitude
         })
-    },function () {
+    }, function () {
 
         alert("Something wrong")
     });
